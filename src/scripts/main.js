0d6a20667e2ecaa720d11e6d4381d9c9735a41a4
Lucas Montezuma <lucas.montezumaa@gmail.com>
2024-02-22 16:30:52 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    diaEmMss = 1000*60*60*24;
+    horaEmMss = 1000*60*60;
+    minutoEmMss = 1000*60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento/diaEmMss);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMss) / horaEmMss);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMss) /  minutoEmMss);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento %  minutoEmMss) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento < 0){
+        document.getElementById('contador').innerHTML = "O evento já iniciou"
+    }
+}, 1000)
+
